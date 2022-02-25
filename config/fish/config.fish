83bf3a3771ec162ac0730a9a782d3c66a00b26e6
karthick <a.karthie+github@gmail.com>
2022-02-25 22:32:26 +0000

    :fire: update config for linux

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,18 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    # set global editor
-     set -gx EDITOR nvim
-     #add custom installation to path var
-fish_add_path -P /usr/local/go/bin
-fish_add_path -aP $HOME/go/bin # local go bin path for pkgs
-end
+# set env vars global
+set -Ux SCRIPT_FEATURE_FLAG_PYTHON 1
+set -Ux TOOLS_VERSION '20210609.155238.6564ac12e6'
+set -Ux EDITOR nvim
+fish_add_path -P $HOME/.cargo/bin
+fish_add_path -P $HOME/google-cloud-sdk/bin
+fish_add_path -P $HOME/.bin
+fish_add_path -P $HOME/go/bin
+fish_add_path -P $HOME/.local/nvim/bin
+
+
+
+
+direnv hook fish | source
+
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/karthickayyapillai/google-cloud-sdk/path.fish.inc' ]; . '/Users/karthickayyapillai/google-cloud-sdk/path.fish.inc'; end
